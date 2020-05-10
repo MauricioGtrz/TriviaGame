32b53b1c121856aaccd5c58dfdfd9ff69f9c803c
Mauricio Gutierrez <mauriciogtrz@g.ucla.edu>
2020-05-09 21:27:55 -0700

    Finished the assignment and the game is fully functional.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,105 @@
-//Timer
-// This code will run as soon as the page loads
-// window.onload = function() {
-//     $("#lap").on("click", recordLap);
-//     $("#stop").on("click", stop);
-//     $("#reset").on("click", reset);
-//     $("#start").on("click", start);
-// };
-  
-//  after 120 seconds, execute the timeUp function
-setTimeout(timeUp, 1000 * 10);
+
+
+function getRadioVal(form, name) {
+    var val;
+    var radios = form.elements[name];
+    
+    for (var i=0, len=radios.length; i<len; i++) {
+        if ( radios[i].checked ) { 
+            val = radios[i].value; 
+            break;
+        }
+    }
+    return val;
+}
+
+setTimeout(timeUp, 1000 * 120);
+
+var intervalId = setInterval(count, 1000);
+
+var time = 120;
+
+function count() {
+    time--;
+    var converted = timeConverter(time);
+    $("#countdown").text(converted);
+
+}
+
+function timeConverter(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+    
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    }
+    else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    
+    return minutes + ":" + seconds;
+}
+
   
 
 function timeUp() {
-    // in the element with an id of time-left add an h2 saying Time's Up!
-    // console log done
-    console.log("done");
-    // $("#time-left").append("<h2>Time's Up!</h2>");
-    console.log("time is up");
-    alert("Times UP!")
 
-    // The following line will play the audio file we linked to above:
-    // audio.play();
+    var correct = 0;
+    var wrong = 0;
+    var unanswered = 0;
+
+    clearInterval(intervalId);
+
+    function getRadioVal(form, name) {
+        var val;
+        var radios = form.elements[name];
+        
+        for (var i=0, len=radios.length; i<len; i++) {
+            if ( radios[i].checked ) { 
+                val = radios[i].value; 
+                break; 
+            }
+        }
+        return val; 
+    }
+
+    var answer1 = getRadioVal( document.getElementById('question1'), 'answer1' );
+    var answer2 = getRadioVal( document.getElementById('question2'), 'answer2' );
+    var answer3 = getRadioVal( document.getElementById('question3'), 'answer3' );
+    var answer4 = getRadioVal( document.getElementById('question4'), 'answer4' );
+    var answer5 = getRadioVal( document.getElementById('question5'), 'answer5' );
+
+    function checkCorrect(answer) {
+        if (answer == "correct"){
+            correct++;
+        }
+        else if (answer == "wrong"){
+            wrong++;
+        }
+        else unanswered++;
+    }
+
+    checkCorrect(answer1);
+    checkCorrect(answer2);
+    checkCorrect(answer3);
+    checkCorrect(answer4);
+    checkCorrect(answer5);
+
+
+    $('#gameContainer').html("<h1>Let's see how you did!</h1><br>");
+    $('#gameContainer').append('<h1>Right answers: ' + correct + "</h2>");
+    $('#gameContainer').append('<h1>Wrong answers: ' + wrong) + "</h2>";
+    $('#gameContainer').append('<h1>Unanswered: ' + unanswered) + "</h2>";
 }
-//true and false
-//Start Button
+
+
+
+window.onload = function() {
+    $("#button").on("click", function () {
+    timeUp();
+    });
+};
